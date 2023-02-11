c442e7743e8db959cf7d7923bf28a56a9c9c463a
greenina <igreen0485@gmail.com>
2023-02-11 17:24:51 +0900

    Add horizontal text animation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import p5 from 'p5';
 import Card from './Card';
 import Images from './Image';
 import color from './assets/color.png'
+// import Bar from './Bar';
 
 import { useState, useEffect, useRef } from 'react';
 import scrolldown from './assets/scrolldown.png'
@@ -27,11 +28,21 @@ const App = () => {
       element.scrollIntoView({ behavior: 'smooth' });
     }
   };
+  
 
   return (
     <div className="main">
       <div className='birthcolor'>
       🎨 Birth Color of Feb. 10 is #EA618E <img className="colorimg"src={color}/>, Rose Red/Deep Blush
+      </div>
+      <div class="bar">
+        <span class="bar_content">
+        엉따는 "엉덩이는 따뜻하게"의 줄임말으로, 동의어로는 "김다영", "주전자🫖", "따엉", 등이 있습니다.
+        </span>
+        <span className='bar_content'>
+        본 웹사이트는, 개발자가 부부부업정도 되는 엉따의 진실된 친구 이놔쏭이 갑자기 삘받아 엉따의 이십대 중반 진입 축하를 위해 갑자기 개발해벌인 누추한 웹사이트입니다. <br/>
+        궁금 안하겠지만 코드는 <a href="https://github.com/greenina/ddoangHBD">이곳</a>에서 확인 가능합니다. 
+        </span>
       </div>
       <div className='canvas'>
         <div ref={canvasRef} />
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,30 @@
 import React from "react";
 import './Card.css';
+import { useState, useEffect } from "react";
 
 const Card = () => {
   // var cardFront=document.getElementsByClassName('cardFront').style.height;
   // var cardInside=document.getElementsByClassName('cardInside').style.height;
   // document.getElementsByClassName('cardFront').style.height=cardInside;
+  const password = "1234";
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState();
+  
+  const submitPassword = (e) => {
+    // console.log("VAL: ", e.target.value);
+    if(input === password){
+      setOpen(true);
+    }
+    else{
+      alert("Wrong Password!");
+    }
+  }
+
+  const enterPassword = (e) => {
+    setInput(e.target.value);
+  }
+
+
   return (
     <div className="birthdayCard">
     <div className="cardFront">
@@ -23,20 +43,28 @@ const Card = () => {
     </div>
     <div className="cardInside">
       <h3 className="back">김다영 이십대 중반 된거 축하해!😜🎉</h3>
-      <p>내가 사랑하는 앙또에게</p>
-      <p>
-        히히 앙또 내가 너모 늦었지ㅎㅎㅎ<br/>
-        어젯밤에 
-        스물세번째 생일 너무너무 츄카행!! 🎉😻🥳 이제 이십대 중반이래요~~<br/>
-        언제 스물셋 된거지 우리 분명 열일곱에 만났자나 그래 안그래 😮
-        요것은 부업 개발짜인만큼 하루 늦은 오늘 초큼 급하게 갑자기 삘받고 얼레벌레 뚝딱뚝딱 만들어봤오ㅎㅎ<br/>
-        근데 약간쓰 더 늦어지고 싶지 않아서 정렬같은건 아직 하나두 안맞지만 고런대로 엉성해보이는 귀여움이 있으니까 히히<br/>
-        그냥 인터랙티브 한.. 이벤트창을 구성했다는 것에 의의를 히히<br/>
-        우리가 둘이 봤을 때 사진을 많이 찍지는 않아서..는 아닌거 같지만 지금 얼레벌레 사진을 빠르게 고르다보니까<br/>
-        맨날 쓰던 사진들을 재탕하는 느낌이 들 수도 있지만 그거슨 분명히 기분탓이야 호호<br/>
-
-
-      </p>
+      {open?
+        <div>
+          <p>내가 사랑하는 앙또에게</p>
+          <p>
+            스물세번째 생일 너무너무 츄카행!! 🎉😻🥳 이제 이십대 중반이래요~~<br/>
+            언제 스물셋 된거지 우리 분명 열일곱에 만났자나 그래 안그래 😮<br/>
+            앙또 생일 담날 아침까지 내야하는거를 미룸대장 송이놔가 미루고 미루다보니 뿌 앙또 생일 축하가 너모 늦어부랴싸<br/> 
+            사죄의 마음을 한가득 담아 이놔쓰 부업 개발짜인만큼 하루 늦은 오늘 초큼 급하게 갑자기 삘받아서 얼레벌레 뚝딱뚝딱 만들어봤오ㅎㅎ<br/>
+            근데 너무 늦어진거가타서.. 정렬같은건 아직 하나두 안맞지만 고런대로 엉성해보이는 귀여움이 있으니까 히히<br/>
+            그냥 인터랙티브 한.. 이벤트창을 구성했다는 것에 의의를 히히<br/>
+            우리가 둘이 봤을 때 사진을 많이 찍지는 않아서..는 아닌거 같지만 지금 얼레벌레 사진을 빠르게 고르다보니까<br/>
+            맨날 쓰던 사진들을 재탕하는 느낌이 들 수도 있지만 그거슨 분명히 기분탓이야 호호<br/>
+            <br/>
+          </p>
+        </div>:
+        <div>
+          <form onSubmit={submitPassword}>
+            <input type="text" value={input} onChange={enterPassword} />
+            <input type="submit" value="제출"/>
+          </form>
+        </div>}
+      
       {/* <p className="name">앙또 트윈 이놔</p> */}
     </div>
   </div>
